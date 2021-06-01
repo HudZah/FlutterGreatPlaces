f4a8f1f83d3c4cc622df2b75dbdd498538d44216
Hudhayfa <chiefhudhayfa@gmail.com>
2021-06-02 00:48:57 +0530

    First commit

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+import '../screens/map_screen.dart';
+import '../helpers/location_helper.dart';
+
+class LocationInput extends StatefulWidget {
+  final Function onSelectPlace;
+
+  LocationInput(this.onSelectPlace);
+
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl;
+
+  void _showPreview(double lat, double lng) {
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+      latitude: lat,
+      longitude: lng,
+    );
+
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  Future<void> _getCurrentUserLocation() async {
+    try {
+      final locData = await Location().getLocation();
+      _showPreview(locData.latitude, locData.longitude);
+
+      widget.onSelectPlace(locData.latitude, locData.longitude);
+    } catch (error) {
+      return;
+    }
+  }
+
+  Future<void> _selectOnMap() async {
+    final LatLng selectedLocation = await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+
+    if (selectedLocation == null) {
+      return;
+    }
+    _showPreview(selectedLocation.latitude, selectedLocation.longitude);
+
+    widget.onSelectPlace(selectedLocation.latitude, selectedLocation.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          height: 170,
+          width: double.infinity,
+          child: _previewImageUrl == null
+              ? Text(
+                  "No location chosen",
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl,
+                  fit: BoxFit.contain,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              icon: Icon(Icons.location_on),
+              label: Text("Current location"),
+              onPressed: _getCurrentUserLocation,
+            ),
+            TextButton.icon(
+              icon: Icon(Icons.map),
+              label: Text("Select on Map"),
+              onPressed: _selectOnMap,
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
